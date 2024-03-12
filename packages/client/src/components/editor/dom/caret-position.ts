4ef07188b16e81ad796659c7584ba7d39e019fcb
uint4096 <abhishek.kumar251095@gmail.com>
2024-03-12 23:11:29 +0000

    feat(editor): track caret position

---
+++ packages/client/src/components/editor/dom/caret-position.ts
@@ -0,0 +1,46 @@
+import { getDescendant } from ".";
+
+const NODE_TEXT_MAP = {
+  span: { type: "nowrap", value: "" },
+  "#text": { type: "nowrap", value: "" },
+  b: { type: "wrap", value: "**" },
+  i: { type: "wrap", value: "*" },
+  s: { type: "wrap", value: "~~" },
+  br: { type: "nowrap", value: "\n" },
+  div: { type: "nowrap", value: "\n" },
+};
+
+export const getCaretPosition = (
+  base: Node,
+  position: string,
+  offset: number
+) => {
+  const elem = getDescendant(base, position);
+  let foundNode = false;
+  const parseNode = (nodeList: NodeList) => {
+    let idx = 0,
+      caretPosition = 0;
+    while (idx < nodeList.length && !foundNode) {
+      const node = nodeList[idx];
+      const nodeText =
+        NODE_TEXT_MAP[<keyof typeof NODE_TEXT_MAP>node.nodeName.toLowerCase()];
+      if (node === elem) {
+        foundNode = true;
+        return caretPosition + offset;
+      } else if (node.nodeName === "#text") {
+        caretPosition += node.nodeValue?.length ?? 0;
+      } else {
+        caretPosition += nodeText.value?.length + parseNode(node.childNodes);
+      }
+
+      if (nodeText?.type === "wrap" && !foundNode) {
+        caretPosition += nodeText?.value.length;
+      }
+      idx++;
+    }
+
+    return caretPosition;
+  };
+
+  return parseNode(base.childNodes);
+};
