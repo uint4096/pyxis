488c7c80fe1292c2f1617e38b97ce2fcf9fd39ae
uint4096 <abhishek.kumar251095@gmail.com>
2024-04-14 22:15:34 +0100

    refactor: removed lexer v0

--- packages/client/src/components/editor/text/transpiler/lexer.ts
+++ packages/client/src/components/editor/text/transpiler/lexer.ts
@@ -28,30 +28,6 @@ type Pattern = {
   startOnly?: boolean; // Should only be considered if the line begins with the token
 };
 
-const getHeader = (text: string) => {
-  let i = 0,
-    token = "";
-  while (i < text.length || text[i] !== "\n") {
-    if (text[i] !== "#") {
-      break;
-    }
-
-    token = `${token}#`;
-    i++;
-  }
-
-  if (i <= 6 && text[i] === " ") {
-    return { isHeader: true, type: `h${i}`, value: token };
-  }
-
-  return { isHeader: false };
-};
-
-const getLink = (text: string) => {
-  const match = text.match(/(\bhttps?:\/\/|\bwww\.)\S+/i);
-  return match?.[0];
-};
-
 const patterns: Readonly<Array<Pattern>> = [
   {
     type: "bold&italic",
@@ -126,173 +102,56 @@ const patterns: Readonly<Array<Pattern>> = [
 ] as const;
 
 export const lexer = (text: string) => {
-  const walk = (text: string) => {
-    const tokens: Array<Token> = [];
-    let i = 0;
-    while (i < text.length) {
-      const txt = text.slice(i);
-      const match = patterns.find(
-        (match) => !!txt.match(new RegExp(match.pattern))?.[0]
-      );
-
-      if (match && (!match.startOnly || i === 0)) {
-        const _content = txt.match(new RegExp(match.pattern));
-        const content = _content?.[0] as NonNullable<string>;
-        tokens.push({ type: match.type, index: i, value: match.value });
-
-        const hasEndNode = !!content.match(
-          new RegExp(`${match.value.replace(/\*/g, "\\*")}$`)
-        )?.[0];
-
-        const textContent = hasEndNode
-          ? content.replace(
-              new RegExp(`${match.value.replace(/\*/g, "\\*")}$`),
-              ""
-            )
-          : content;
-
-        if (match.textOnly) {
-          tokens.push({ type: "text", index: i, value: textContent });
-        } else {
-          tokens.push(...walk(textContent));
-        }
-
-        if (hasEndNode || match.forceEnd) {
-          tokens.push({
-            type: match.type,
-            index: i + match.value.length + textContent.length,
-            value: match.value,
-          });
-        }
-
-        i +=
-          (hasEndNode ? 2 * match.value.length : match.value.length) +
-          textContent.length;
-      } else {
-        const lastToken = tokens[tokens.length - 1];
-        if (lastToken?.type === "text") {
-          lastToken.value = lastToken.value + text[i];
-        } else {
-          tokens.push({ type: "text", index: i, value: text[i] });
-        }
-        i += 1;
-      }
-    }
-
-    return tokens;
-  };
-
-  return walk(text);
-};
-
-export const lexer_v0 = (text: string) => {
-  const syntaxTokens: Array<Token> = [];
+  const tokens: Array<Token> = [];
   let i = 0;
-
   while (i < text.length) {
-    switch (text[i]) {
-      case "*":
-      case "_": {
-        if (text[i + 1] === text[i] && text[i + 2] === text[i]) {
-          if (
-            (text[i - 1] !== undefined && text[i - 1] !== " ") ||
-            (text[i + 3] !== undefined && text[i + 3] !== " ")
-          ) {
-            syntaxTokens.push({
-              type: "bold&italic",
-              index: i,
-              value: `${text[i]}${text[i]}${text[i]}`,
-            });
-          }
+    const txt = text.slice(i);
+    const match = patterns.find(
+      (match) => !!txt.match(new RegExp(match.pattern))?.[0]
+    );
 
-          i += 2;
-        } else if (text[i + 1] === text[i]) {
-          syntaxTokens.push({
-            type: "bold",
-            index: i,
-            value: `${text[i]}${text[i]}`,
-          });
-          i += 1;
-        } else {
-          syntaxTokens.push({ type: "italic", index: i, value: `${text[i]}` });
-        }
+    if (match && (!match.startOnly || i === 0)) {
+      const content = txt.match(new RegExp(match.pattern))?.[0] as NonNullable<string>;
+      tokens.push({ type: match.type, index: i, value: match.value });
 
-        break;
+      const hasEndNode = !!content.match(
+        new RegExp(`${match.value.replace(/\*/g, "\\*")}$`)
+      )?.[0];
+
+      const textContent = hasEndNode
+        ? content.replace(
+            new RegExp(`${match.value.replace(/\*/g, "\\*")}$`),
+            ""
+          )
+        : content;
+
+      if (match.textOnly) {
+        tokens.push({ type: "text", index: i, value: textContent });
+      } else {
+        tokens.push(...lexer(textContent));
       }
-      case "~": {
-        if (text[i + 1] === "~")
-          syntaxTokens.push({ type: "strikethrough", index: i, value: "~~" });
-        i += 1;
-        break;
-      }
-      case "`": {
-        syntaxTokens.push({ type: "code", index: i, value: "`" });
-        break;
-      }
-      case "#": {
-        if (i !== 0 && text[i - 1] !== "\n") {
-          break;
-        }
 
-        const { isHeader, type, value } = getHeader(text);
-        if (!isHeader) {
-          break;
-        }
-
-        syntaxTokens.push({
-          type: <NonNullable<Tokens>>type,
-          index: i,
-          value: <NonNullable<string>>value,
-        });
-
-        i += (<NonNullable<string>>value)?.length - 1;
-
-        break;
-      }
-      case "h": {
-        const link = getLink(text.slice(i));
-        if (!link) {
-          break;
-        }
-
-        syntaxTokens.push({
-          type: "link",
-          index: i,
-          value: "",
-        });
-
-        // Closing token so that text is extracted correctly
-        syntaxTokens.push({
-          type: "link",
-          index: i + link.length,
-          value: "",
-        });
-
-        i += link.length - 1;
-      }
-    }
-
-    i += 1;
-  }
-
-  // Insert text nodes
-  const tokens = syntaxTokens.reduce<Array<Token>>(
-    (tokens, syntaxToken, index) => {
-      tokens.push(syntaxToken);
-
-      const textStart = syntaxToken.index + syntaxToken.value.length;
-      if (textStart !== syntaxTokens[index + 1]?.index) {
+      if (hasEndNode || match.forceEnd) {
         tokens.push({
-          type: "text",
-          index: textStart,
-          value: text.slice(textStart, syntaxTokens[index + 1]?.index),
+          type: match.type,
+          index: i + match.value.length + textContent.length,
+          value: match.value,
         });
       }
 
-      return tokens;
-    },
-    [{ type: "text", index: 0, value: text.slice(0, syntaxTokens[0]?.index) }]
-  );
+      i +=
+        (hasEndNode ? 2 * match.value.length : match.value.length) +
+        textContent.length;
+    } else {
+      const lastToken = tokens[tokens.length - 1];
+      if (lastToken?.type === "text") {
+        lastToken.value = lastToken.value + text[i];
+      } else {
+        tokens.push({ type: "text", index: i, value: text[i] });
+      }
+      i += 1;
+    }
+  }
 
   return tokens;
 };
