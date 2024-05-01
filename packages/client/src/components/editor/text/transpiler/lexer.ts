06984ac5fe79bbad2587fdeaf46391399d088515
uint4096 <abhishek.kumar251095@gmail.com>
2024-05-02 00:21:50 +0100

    feat(editor): use custom pattern matcher

--- packages/client/src/components/editor/text/transpiler/lexer.ts
+++ packages/client/src/components/editor/text/transpiler/lexer.ts
@@ -1,19 +1,5 @@
-import { patternMatcher } from "../../pattern-matcher";
-
-export type Tokens =
-  | "text"
-  | "bold&italic"
-  | "bold"
-  | "italic"
-  | "strikethrough"
-  | "code"
-  | "h1"
-  | "h2"
-  | "h3"
-  | "h4"
-  | "h5"
-  | "h6"
-  | "link";
+import { matchContent } from "../pattern";
+import { Tokens } from "../types";
 
 export type Token = {
   type: Tokens;
@@ -22,114 +8,17 @@ export type Token = {
   position?: "start" | "end";
 };
 
-type Pattern = {
-  type: Tokens;
-  pattern: RegExp;
-  value: string;
-  secondaryPattern?: RegExp;
-  textOnly?: boolean; // Should not contain any child elements
-  forceEnd?: boolean; // Force adding a closing token after parsing an element
-  startOnly?: boolean; // Should only be considered if the line begins with the token
-};
-
-const patterns: Readonly<Array<Pattern>> = [
-  {
-    type: "text",
-    pattern: new RegExp("^(?!.*[~*`#]|.*(?:http[s]?://|www\.)).*$"),
-    value: "" 
-  },
-  {
-    type: "code",
-    pattern: /(?<=^`)(.*?)(?=`|$)/,
-    secondaryPattern: /(?<=^`)(.*?)$/,
-    value: "`",
-    textOnly: true,
-  },
-  {
-    type: "link",
-    pattern: /(^https?:\/\/|^www\.)\S+/i,
-    value: "",
-    textOnly: true,
-    forceEnd: true,
-  },
-  {
-    type: "h1",
-    pattern: /(?<=^#(?=[^#]))\s.*/,
-    value: "#",
-    textOnly: true,
-    startOnly: true,
-  },
-  {
-    type: "h2",
-    pattern: /(?<=^##(?=[^#]))\s.*/,
-    value: "##",
-    textOnly: true,
-    startOnly: true,
-  },
-  {
-    type: "h3",
-    pattern: /(?<=^###(?=[^#]))\s.*/,
-    value: "###",
-    textOnly: true,
-    startOnly: true,
-  },
-  {
-    type: "h4",
-    pattern: /(?<=^####(?=[^#]))\s.*/,
-    value: "####",
-    textOnly: true,
-    startOnly: true,
-  },
-  {
-    type: "h5",
-    pattern: /(?<=^#####(?=[^#]))\s.*/,
-    value: "#####",
-    textOnly: true,
-    startOnly: true,
-  },
-  {
-    type: "h6",
-    pattern: /(?<=^######(?=[^#]))\s.*/,
-    value: "######",
-    textOnly: true,
-    startOnly: true,
-  },
-  {
-    type: "bold&italic",
-    pattern: /(?<=^\*\*\*(?=[^\s]))(.*?)(?=(?<!\s)\*\*\*)/,
-    secondaryPattern: /(?<=^\*\*\*(?=[^\s]))(.*?)$/,
-    value: "***",
-  },
-  {
-    type: "bold",
-    pattern: /(?<=^\*\*(?=[^\s]))(.*?)(?=(?<!\s)\*\*)/,
-    secondaryPattern: /(?<=^\*\*(?=[^\s|*]))(.*?)$/,
-    value: "**",
-  },
-  {
-    type: "italic",
-    pattern: /(?<=^\*(?=[^\s|*]))(.*?)(?=(?<![\s|*])\*)/,
-    secondaryPattern: /(?<=^\*(?=[^\s|*]))(.*?)$/,
-    value: "*",
-  },
-  {
-    type: "strikethrough",
-    pattern: /(?<=^~~(?=[^\s]))(.*?)(?=(?<!\s)~~)/,
-    secondaryPattern: /(?<=^~~(?=[^\s]))(.*?)$/,
-    value: "~~",
-  },
-] as const;
-
 export const lexer = (text: string, idx = 0) => {
   const tokens: Array<Token> = [];
   let i = 0;
 
   while (i < text.length) {
     const txt = text.slice(i);
-    const match = patternMatcher(txt);
+    const match = matchContent(txt);
 
     if (
-      match && match.type !== 'text' &&
+      match &&
+      match.type !== "text" &&
       (!match.startOnly || (match.startOnly && idx === 0 && i === 0))
     ) {
       const content = match.capture;
