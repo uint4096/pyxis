48644bdc59e0ce70399c400001dfd554fa06c59e
uint4096 <abhishek.kumar251095@gmail.com>
2024-05-09 01:18:49 +0100

    feat(app): ffi for config access

--- packages/app/src-tauri/src/ffi.rs
+++ packages/app/src-tauri/src/ffi.rs
@@ -1,8 +1,64 @@
+use crate::config::system::SystemConfig;
+
+use super::config::{
+    configuration::Configuration,
+    store::{Store, StoreConfig},
+    system::System,
+    workspaces::{Workspace, WorkspaceConfig},
+};
 use super::reader::FileContent;
-use super::workspace::store;
 
 #[tauri::command]
-pub fn read_store_config (path: String) -> FileContent {
-    let store = store::Store(&path);
+pub fn read_store_config(path: String) -> FileContent {
+    let store = Store(&path);
     store.get_config()
+}
+
+#[tauri::command]
+pub fn write_store_config(path: String, config: StoreConfig) -> bool {
+    let store = Store(&path);
+    store.save_config(config)
+}
+
+#[tauri::command]
+pub fn read_workspace_config(path: String) -> FileContent {
+    let workspace = Workspace(&path);
+    workspace.get_config()
+}
+
+#[tauri::command]
+pub fn write_workspace_config(path: String, config: WorkspaceConfig) -> bool {
+    let workspace = Workspace(&path);
+    workspace.save_config(config)
+}
+
+#[tauri::command]
+pub fn read_system_config() -> FileContent {
+    if let Some(home_dir) = dirs::home_dir() {
+        let system = System(
+            &home_dir
+                .to_str()
+                .expect("[Config Error!] Failed to convert dir to string!"),
+        );
+        system.get_config()
+    } else {
+        println!("[Config Error!] Failed to retrieve home directory!");
+        FileContent::new_failed()
+    }
+}
+
+#[tauri::command]
+pub fn write_system_config(config: SystemConfig) -> bool {
+    if let Some(home_dir) = dirs::home_dir() {
+        let system = System(
+            &home_dir
+                .to_str()
+                .expect("[Config Error!] Failed to convert dir to string!"),
+        );
+
+        system.save_config(config)
+    } else {
+        println!("[Config Error!] Failed to retrieve home directory!");
+        false
+    }
 }
--- packages/app/src-tauri/src/main.rs
+++ packages/app/src-tauri/src/main.rs
@@ -1,21 +1,31 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
-mod workspace;
+mod config;
+mod ffi;
 mod reader;
 mod writer;
-mod ffi;
-use crate::ffi::read_store_config;
+use crate::ffi::{
+    read_store_config, read_system_config, read_workspace_config, write_store_config,
+    write_system_config, write_workspace_config,
+};
 use tauri::{App, Manager};
 
 fn main() {
-  tauri::Builder::default()
-    .invoke_handler(tauri::generate_handler![read_store_config])
-    .setup(|app: &mut App| {
-      let window = app.get_window("main").expect("Failed to get main window!");
-      // Doing this in tauri config does not allow super + arrow keys to work
-      let _ = window.maximize();
-      Ok(())
-    })
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![
+            read_store_config,
+            write_store_config,
+            read_workspace_config,
+            write_workspace_config,
+            read_system_config,
+            write_system_config
+        ])
+        .setup(|app: &mut App| {
+            let window = app.get_window("main").expect("Failed to get main window!");
+            // Doing this in tauri config does not allow super + arrow keys to work
+            let _ = window.maximize();
+            Ok(())
+        })
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
